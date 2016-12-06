b677ab0bc61a6d4d5cacf1d36bd0b0a3a915c6f4
Rogelio Negrete <rogelio_negrete@live.com>
2016-12-05 20:58:04 -0800

    Add files via upload

--- complete/Part2Checks.py
+++ complete/Part2Checks.py
@@ -1,3 +1,12 @@
+def checkForPrintMisspelling(input_list):
+    for word in input_list:
+        # the way input_list is formatted then a misspelled "print" will still have left parantheses next to it
+        # e.g. pabct(
+        if len(word) == 6 and word[0] == 'p' and word[-2] == 't' and word[-1] == '(':
+            # we definitely know this word is misspelled from the input_list
+            print('print is misspelled.')
+            exit(1)
+
 def checkForPeriod(input_list):
     last_word = input_list[-1]
     if last_word == 'end':
@@ -48,8 +57,45 @@ def checkForMissingSemiColons():
                 print('; is missing from the following code of line:\t' + line)
                 exit(1)
 
-def checkForMissingCommas():
-    filelines = open('finalv2.txt').readlines()
-    filelines = [line.strip() for line in filelines]  # strip excess whitespace just in case
+def checkForMissingCommas(input_list):
 
-    for
+    var_index = input_list.index('var')
+    integer_index = input_list.index('integer')
+    variable_list = input_list[var_index + 1:integer_index]
+
+    i=0
+    while(i < len(variable_list)):
+        word = variable_list[i]
+        peak_word = variable_list[i+1]
+        if word not in [',', ':']:
+            # word is a some var, we need to check if the next word is a , or :
+            if peak_word not in [',', ':']:
+                print(', is missing from var line.')
+                exit(1)
+            else:
+                i += 2
+
+def checkForParentheses(input_list):
+    begin_index = input_list.index('begin')
+    end_index = input_list.index('end.')
+    word_list = input_list[begin_index + 1:end_index]  # get our words to examine from 'begin' and onwards
+
+    triggered_parentheses = False
+    for word in word_list:
+        # once we see a (, then we need to look for a )
+        if word == '(':
+            if triggered_parentheses:
+                # we found another left parantheses without finding a right one first
+                print('Right parantheses is missing.')
+                exit(1)
+            else:
+                triggered_parentheses = True
+        elif word == ')':
+            # only if it was set
+            if triggered_parentheses:
+                # we found a closing right parentheses, so reset the flag
+                triggered_parentheses = False
+            else:
+                # we found another right parantheses without finding a left one first
+                print('Left Parentheses is missing.')
+                exit(1)
